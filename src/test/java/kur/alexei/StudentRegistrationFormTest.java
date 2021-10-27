532707a1b2270e0a5b3e18dfd518a682a59c1784
Alekur333 <amastar963@gmail.com>
2021-10-27 11:17:29 +0500

    first homework finished

--- src/test/java/kur/alexei/StudentRegistrationFormTest.java
+++ src/test/java/kur/alexei/StudentRegistrationFormTest.java
@@ -4,18 +4,21 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.*;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
 
 public class StudentRegistrationFormTest {
     String firstName = "Alexei";
     String lastName = "Kurochkin";
     String email = "my@mail.com";
     String gender = "Male";
-    int mobile = 1234567890;
-    String dateOfBirth = "03.30.1974";
-    String subjects = "comp";
-    String hobbies = "Music";
+    String mobile = "1234567890";
+    String subject = "comp";
+    File filePath = new File("/Users/aleks/Desktop/filePath.png");
     String currentAddress = "Students Street 9";
     String state = "NCR";
     String city = "Delhi";
@@ -23,8 +26,6 @@ public class StudentRegistrationFormTest {
 
     @BeforeAll
     public static void beforeTest() {
-        //Configuration.timeout=30000;
-        //Configuration.pageLoadTimeout=50000;
         Configuration.startMaximized=true;
     }
 
@@ -35,6 +36,29 @@ public class StudentRegistrationFormTest {
 
     @Test
     void name() {
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        //$("#gender-radio-1").click();
+        $(byText("Male")).click();
+        $("#userNumber").setValue(mobile);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-dropdown-container").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1974");
+        $(".react-datepicker__month-dropdown-container").click();
+        $(".react-datepicker__month-select").selectOptionByValue("2");
+        $x("//div[text()=\"30\"]").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $(byText("Music")).click();
+        $("#uploadPicture").uploadFile(filePath);
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
+        $("#submit").click();
+
+        $(".table").shouldHave(text(firstName), text(lastName), text(email),
+                text("Male"), text(mobile), text("30 March,1974"), text("Computer Science"),
+                text("Music"), text("filePath.png"), text(currentAddress), text(state), text(city));
 
     }
 }
